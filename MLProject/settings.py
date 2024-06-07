f53f3dcbe32d8b658f389f5de912cb47d4fce883
SUBHAN SAHEB SHAIK <122206043+subhanSahebShaik@users.noreply.github.com>
2024-06-07 17:58:16 +0530

    Update settings.py

--- MLProject/settings.py
+++ MLProject/settings.py
@@ -76,10 +76,10 @@ WSGI_APPLICATION = 'MLProject.wsgi.application'
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    # 'default': {
+    #     'ENGINE': 'django.db.backends.sqlite3',
+    #     'NAME': BASE_DIR / 'db.sqlite3',
+    # }
 }
 
 
@@ -118,6 +118,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
 STATIC_URL = '/mp2/static/'
+STATICFILES_DIRS = [BASE_DIR/'static',]
+STATIC_ROOT = BASE_DIR/'staticfiles'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
